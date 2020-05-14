a632b313b7f71c88071e6233ad0e2e1bd622f5c6
Brad Fujimoto <bradjordan@gmail.com>
2020-05-14 16:51:26 +0200

    thursday done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#Ingredient.create(name: "lemon")
+#Ingredient.create(name: "ice")
+#Ingredient.create(name: "mint leaves")
+
+require 'json'
+require 'open-uri'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredient_list = open(url).read
+ingredient = JSON.parse(ingredient_list)
+
+ingredient["drinks"].each do |ingredient|
+  Ingredient.create!(name: ingredient["strIngredient1"])
+end
